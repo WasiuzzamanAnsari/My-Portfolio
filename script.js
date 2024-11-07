7e08be6b3770cf7e3d3c411a2d2152ad7a11cca4
WasiuzzamanAnsari <ansariwasi117@gmail.com>
2024-11-07 19:43:15 +0530

    Added the files

---
+++ script.js
@@ -0,0 +1,9 @@
+function myMenuFunction(){
+    let menuBtn = document.getElementById("myNavMenu");
+  
+    if(menuBtn.className === "nav-menu"){
+      menuBtn.className += " responsive";
+    } else {
+      menuBtn.className = "nav-menu";
+    }
+  }
